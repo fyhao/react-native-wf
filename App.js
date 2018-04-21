36348b243a2ae0c09bee19c76cc4a54d9ac2ee14
fyhao <fyhao1@gmail.com>
2018-04-21 18:03:34 +0800

    apply listview componentdidmount

--- App.js
+++ App.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, ListView, View } from 'react-native';
 import Constants from './lib/Constants';
 var API_URL = Constants.API_URL;
 export default class App extends React.Component {
+  
+  componentDidMount() {
+        this.setState({
+			
+          title: 'HN Reader',
+          dataSource: new ListView.DataSource({
+            rowHasChanged: (row1, row2) => row1 !== row2,
+          }),
+          news: {},
+          loaded: false
+        
+		})   
+    }
+  
   render() {
     return (
       <View style={styles.container}>
