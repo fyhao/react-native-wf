6fdff343b555723557505f99ff36de63ec2dced8
fyhao <fyhao1@gmail.com>
2018-04-21 17:15:18 +0800

    Constants.API_URL

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import Constants from './lib/Constants';
+var API_URL = Constants.API_URL;
 export default class App extends React.Component {
   render() {
     return (
@@ -8,6 +9,7 @@ export default class App extends React.Component {
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+		<Text>API URL: {API_URL}</Text>
       </View>
     );
   }
