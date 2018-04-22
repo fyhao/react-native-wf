70044f68885c9e2c3bdadd120c77d9758e380130
fyhao <fyhao1@gmail.com>
2018-04-22 13:19:22 +0800

    init detox

--- scripts/install-integration-test-ios.sh
+++ scripts/install-integration-test-ios.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 echo "Inside install-integration-test"
 npm install
+brew tap wix/brew
+brew install applesimutils
+npm install -g detox-cli
+npm install detox --save-dev
--- scripts/script-integration-test-ios.sh
+++ scripts/script-integration-test-ios.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "Inside script-integration-test-ios"
-npm test
+detox build
+detox test
